a712ac3af88a91f16d0fb0f8c4cc05418290a460
unknown <salmanmal@yahoo.com>
2018-08-19 01:30:09 +0530

    server.js changes

--- server.js
+++ server.js
@@ -29,3 +29,6 @@ var server = app.listen(process.env.PORT || 3000, function () {
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/build')));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/build','/index.html'));
+});
